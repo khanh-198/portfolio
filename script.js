a46b55d8ac4b4c1badc5f5a0e633ef0b0e54715b
Khanh-Ha <bkha@uw.edu>
2024-10-21 12:45:41 -0700

    update website

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let is_dark = 0;
 
 navbar_element.forEach(nav => {
     nav.addEventListener('mouseover', function(){
-        nav.style.textDecoration = 'line-through';
+        nav.style.textDecoration = 'underline';
     });
     nav.addEventListener('mouseout', function(){
         nav.style.textDecoration = '';
@@ -19,20 +19,22 @@ function darkmode(){
     if (is_dark % 2 == 0){
         document.body.style.backgroundColor = 'black';
         document.body.style.color = 'white';
-        document.getElementById('github').src = 'assets/github-logo-dark.png';
+        document.getElementById('github').src = 'assets/github-mark-white.png';
+        document.getElementById('linkedin').src = 'assets/In-White-128@2x.png';
         navbar_element.forEach(nav => {
             nav.style.color = 'white';
         });
-        dark_switch.innerHTML = '----->light<-----';
+        dark_switch.innerHTML = 'Light';
     }
     else {
         document.body.style.backgroundColor = 'white';
         document.body.style.color = 'black';
-        document.getElementById('github').src = 'assets/github-logo.png';
+        document.getElementById('github').src = 'assets/github-mark.png';
+        document.getElementById('linkedin').src = 'assets/In-Blue-128.png';
         navbar_element.forEach(nav => {
             nav.style.color = 'black';
         });
-        dark_switch.innerHTML = '----->dark<-----';
+        dark_switch.innerHTML = 'Dark';
 
 
     }
